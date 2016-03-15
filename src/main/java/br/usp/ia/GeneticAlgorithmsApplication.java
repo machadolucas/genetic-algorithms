3418c6bb9586002d2e21bebbfea711d964bb1d0f
Lucas Machado <lmachado@uolinc.com>
2016-03-15 10:38:38 -0300

    first commit

---
+++ src/main/java/br/usp/ia/GeneticAlgorithmsApplication.java
@@ -0,0 +1,12 @@
+package br.usp.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneticAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneticAlgorithmsApplication.class, args);
+	}
+}
