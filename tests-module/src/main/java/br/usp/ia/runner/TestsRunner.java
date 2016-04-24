4b65475ed88088e118067fe42252a8d80bbbe9cd
Lucas Machado <machadolucas@me.com>
2016-04-24 19:23:58 -0300

    Execucao paralela dos testes funcionando. Basta executar o modulo de testes com o comando run

--- tests-module/src/main/java/br/usp/ia/runner/TestsRunner.java
+++ tests-module/src/main/java/br/usp/ia/runner/TestsRunner.java
@@ -3,8 +3,13 @@ package br.usp.ia.runner;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Component
 public class TestsRunner {
@@ -17,11 +22,24 @@ public class TestsRunner {
         taskExecutor.initialize();
 
         //TODO obter a lista com todos os caminhos dos arquivos de teste.
-        final List<String> testPaths = new LinkedList<>();
+
+        final List<Path> testPaths = new LinkedList<>();
+
+        try (Stream<Path> filePathStream = Files.walk(Paths.get("tests/"))) {
+            filePathStream.forEach(filePath -> {
+                if (Files.isRegularFile(filePath)) {
+                    testPaths.add(filePath);
+                }
+            });
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
 
         testPaths.forEach(path -> {
             final TestTask task = new TestTask(path);
             taskExecutor.execute(task);
         });
+
+        taskExecutor.shutdown();
     }
 }
